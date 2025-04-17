64e964033d981eb75c71696d9ae98b965acf37d9
nata2627 <gordeevanatalia04@gmail.com>
2025-04-17 13:10:28 +0300

    delete un files

--- label_data/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from label_data.utils.labeler import AngliсismLabeler
-from label_data.utils.analyzer import analyze_anglicisms
-from label_data.utils.logger import CustomLogger
-
-__all__ = [
-    'AngliсismLabeler',
-    'analyze_anglicisms',
-    'CustomLogger'
-]
